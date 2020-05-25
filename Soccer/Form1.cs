4e804c3a0ebf2e419a0a8a662854c59a7c0631bc
YuraMikhaylov <recon.97@mail.ru>
2020-05-26 00:36:59 +0300

    Initial commit

---
+++ Soccer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soccer
+{
+    public partial class Form1 : Form
+    {
+        public Entity ball;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        public void Init()
+        {
+            ball = new Entity(new Size(50, 50));
+
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            ball.myPhysics.ApplyPhysics();
+            Invalidate();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            ball.DrawSprite(g);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ball.myPhysics.AddForceQuickly(0.2f);
+        }
+    }
+}
---
+++ Soccer/Physics.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soccer
+{
+    public class Physics
+    {
+        public PointF position;
+        float gravity;
+        float a;
+        float impulse;
+
+        public float dx;
+
+        public Physics()
+        {
+            position = new PointF(100, 150);
+            gravity = 0;
+            a = 0.4f;
+            impulse = -15;
+            dx = -1;
+        }
+
+        public void ApplyPhysics()
+        {
+            CalculatePhysics();
+        }
+
+        public void CalculatePhysics()
+        {
+            
+
+            if (position.X > 750 && dx > 0)
+            {
+                dx /= 2;
+                dx *= -1;
+            }
+            if (position.X < 0 && dx<0)
+            {
+                dx /= 2;
+                dx *= -1;
+            }
+            position.X += dx;
+
+            if (position.Y < 500 || a<0)
+            {
+                if (a != 0.4f)
+                {
+                    a += 0.05f;
+                }
+                position.Y += gravity;
+                
+                gravity += a;
+            }
+            else
+            {
+                if (position.Y >= 500)
+                {
+                    if (Math.Abs(impulse) > 0.1f)
+                    {
+                        impulse /= 2;
+                        AddForce(a/2);
+                    }
+                }
+            }
+        }
+
+
+        public void AddForce(float forceValue)
+        {
+            gravity=-gravity;
+            gravity /= 2;
+            a = -forceValue;
+        }
+
+        public void AddForceQuickly(float forceValue)
+        {  
+            gravity = forceValue*30;
+            gravity *= -1;
+            a = -forceValue;
+            impulse = -15;
+            dx += 5;
+        }
+
+    }
+}
